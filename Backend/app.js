5fd2288bd30b6da7f5de462a6b6e33d24f37d41a
Ankit Mohanty <117025304+Ankitmohanty2@users.noreply.github.com>
2023-09-27 09:38:13 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -10,44 +10,44 @@ const paymentMethodRouter = require("./controllers/paymentMethod");
 const bookingRouter = require("./controllers/booking");
 const spaceRouter = require("./controllers/spaceRouter");
 const cors = require('cors');
-const reviewRouter = require("./controllers/review");
 const dotenv = require("dotenv").config();
 
 // Set body-parser
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
-
-app.use(cors())
+// Enable CORS
+app.use(cors());
 
 // Connect Database
 connectDB();
 
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello world!' });
+});
 
-app.get('/', isLoggedIn, async (req, res) => {
-    res.json({ message: 'Hello world!'})
-})
+// Use routers for different resources
+app.use("/user", userRouter);
+app.use("/parking", parkingRouter);
+app.use("/paymentMethod", paymentMethodRouter);
+app.use("/booking", bookingRouter);
+app.use("/space", spaceRouter);
+app.use("/review", reviewRouter); // Make sure 'reviewRouter' is imported
 
-app.use("/user", userRouter)
-app.use("/parking", parkingRouter)
-app.use("/paymentMethod", paymentMethodRouter)
-app.use("/booking", bookingRouter)
-app.use("/space", spaceRouter)
-app.use("/review", reviewRouter)
-
-// Error handler
-
+// Error handling middleware
 app.use((req, res, next) => {
-    const error = new Error("Not Found")
+    const error = new Error("Not Found");
     error.status = 404;
-    next(error)
-})
+    next(error);
+});
 
 app.use((error, req, res, next) => {
     handleError(error, res);
-})
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
-})
+    console.log(`Running on http://localhost:${port}`);
+});
