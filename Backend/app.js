2d553b7cbf93aa256a7078e263dce874888ac240
Ankit Mohanty <117025304+Ankitmohanty2@users.noreply.github.com>
2023-09-27 09:59:13 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -10,44 +10,43 @@ const paymentMethodRouter = require("./controllers/paymentMethod");
 const bookingRouter = require("./controllers/booking");
 const spaceRouter = require("./controllers/spaceRouter");
 const cors = require('cors');
-const dotenv = require("dotenv").config();
+const reviewRouter = require("./controllers/review");
 
 // Set body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-// Enable CORS
-app.use(cors());
+const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 // Connect Database
 connectDB();
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello world!' });
-});
 
-// Use routers for different resources
-app.use("/user", userRouter);
-app.use("/parking", parkingRouter);
-app.use("/paymentMethod", paymentMethodRouter);
-app.use("/booking", bookingRouter);
-app.use("/space", spaceRouter);
-app.use("/review", reviewRouter); // Make sure 'reviewRouter' is imported
+app.get('/', isLoggedIn, async (req, res) => {
+    res.json({ message: 'Hello world!'})
+})
 
-// Error handling middleware
+app.use("/user", userRouter)
+app.use("/parking", parkingRouter)
+app.use("/paymentMethod", paymentMethodRouter)
+app.use("/booking", bookingRouter)
+app.use("/space", spaceRouter)
+app.use("/review", reviewRouter)
+
+// Error handler
+
 app.use((req, res, next) => {
-    const error = new Error("Not Found");
+    const error = new Error("Not Found")
     error.status = 404;
-    next(error);
-});
+    next(error)
+})
 
 app.use((error, req, res, next) => {
     handleError(error, res);
-});
-
-const port = process.env.PORT || 5000;
+})
 
 app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`);
-});
+    console.log(`Running on http://localhost:${port}`)
+})
